0791f03691ba91dcd92a0b8fc3c5483bcb079747
DonGatito25 <user919387312@gmail.com>
2024-12-17 17:03:45 -0700

    Stuff.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
   return <>
     <Carousel count={Math.ceil(self.innerWidth/200)<10?Math.ceil(self.innerWidth/200):10}/>
     <div className={styles.banner}>
-      <img className={styles.image} src="https://media-public.canva.com/ADwTE/MAGHDfADwTE/1/s.png" alt="Black Man" draggable="false"/>
+      <img className={styles.image} src="../../" alt="Black Man" draggable="false"/>
       <div className={styles.column}>
         <h1 className={styles.title}>Business solutions</h1>
         <p className={styles.text}>Slide for Von dotn slime ur homye. Praesent dui leo, euismod et purus eu, iaculis luctus mauris. Cras a dolor eget nisi aliquet pulvinar eu ut elit. Suspendisse dictum magna quis metus pulvin.</p>
